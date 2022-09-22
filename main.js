8abceee46845c287871ce3e660622b904f32ce32
Caroline-Patrick <carolinespatrick@gmail.com>
2022-09-21 21:07:50 -0500

    made mastermind game

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+let colors = require('colors');
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -28,13 +29,50 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const generateHint = () =>  {
-  // your code here
+const generateHint = (guess) =>  {
+    let solutionArray = solution.split(''); // 'abcd' to ['a', 'b', 'c', 'd']
+    let guessArray = guess.split(''); // 'feih' to ['f', 'e', 'i', 'h']
+    let correctLetterLocations = 0;
+    let correctLetters = 0;
+    let hint
+
+    for (let i = 0; i < solutionArray.length; i++) {
+  
+        if(solutionArray[i] === guessArray[i]) {
+           correctLetterLocations++; // could also be written correctLetterLocations = correctLetterLocations + 1
+           solutionArray[i] = null
+        }
+        }
+
+    for (let j = 0; j < guessArray.length; j++) {
+        if(solutionArray.indexOf(guessArray[j])!=-1){
+            correctLetters++
+      }
+    }  
+    
+    hint = `||${guess} || Hint: You have ${colors.red(correctLetterLocations, 'correct letter(s) in the correct location(s)')} - You have ${colors.yellow(correctLetters, 'letter(s) that are correct, but in the wrong spot')}`
+
+    board.push(hint)
+    // console.log(board)
+
+    if(board.length>10) {
+      console.log("Sorry, you don't have any more guesses left")
+      console.log(`The correct answer was: ${solution}`)
+      board = [];
+      generateSolution()
+    }
 }
 
+
 const mastermind = (guess) => {
-  solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
+  // solution = 'abcd'; // Comment this out to generate a random solution
+  
+  if(guess === solution) 
+    { console.log( "You guessed it!")}
+
+  else {
+    generateHint(guess)
+  }
 }
 
 
